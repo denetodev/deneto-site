44d5b24585dc4efe2c7676cdc1c93fc4b04949eb
Torres-Neto <torresneto.tn@gmail.com>
2025-05-02 17:00:39 -0300

    Inserido projetos reais na sessão de Projetos

--- src/shared/interfaces/projects.data.ts
+++ src/shared/interfaces/projects.data.ts
@@ -3,29 +3,42 @@ import { Project } from './project.interface';
 export const PROJECTS: Project[] = [
   {
     id: '1',
-    title: 'Projeto 1',
-    description: 'Descrição do Projeto 1',
-    image: '../../assets/images/card-challenge/projeto1.jpeg',
-    githubUrl: 'https://github.com/seuusuario/projeto1',
-    siteUrl: 'https://seusite.com/projeto1',
+    title: 'Vatican Legacy',
+    description:
+      'Landing page responsiva em homenagem ao Papa Francisco, com design moderno e estrutura organizada para destacar sua trajetória de forma elegante e envolvente.',
+    image: '../../assets/images/card-challenge/vatican-legacy.png',
+    githubUrl: 'https://github.com/denetodev/vatican-legacy',
+    siteUrl: 'https://vatican-legacy.vercel.app/',
     isHovered: false,
   },
   {
     id: '2',
-    title: 'Projeto 2',
-    description: 'Descrição do Projeto 2',
-    image: 'assets/images/card-challenge/projeto2.jpg',
-    githubUrl: 'https://github.com/seuusuario/projeto2',
-    siteUrl: 'https://seusite.com/projeto2',
+    title: 'Carbon Sphere',
+    description:
+      'Landing page moderna para uma plataforma que une blockchain e IA, promovendo o acesso democrático ao mercado de créditos de carbono. Conecta empresas, projetos ambientais e investidores em um ecossistema digital.',
+    image: '../../assets/images/card-challenge/carbon-sphere.png',
+    githubUrl: 'https://github.com/denetodev/Carbon-Sphere-LandingPage',
+    siteUrl: 'https://carbon-sphere.vercel.app/home',
     isHovered: false,
   },
   {
     id: '3',
-    title: 'Projeto 3',
-    description: 'Descrição do Projeto 3',
-    image: 'assets/images/card-challenge/projeto3.jpg',
-    githubUrl: 'https://github.com/seuusuario/projeto3',
-    siteUrl: 'https://seusite.com/projeto3',
+    title: 'Bistro Bliss',
+    description:
+      'Site moderno e funcional para um restaurante fictício, com design elegante, navegação de cardápio, reservas online e blog para uma experiência digital completa.',
+    image: '../../assets/images/card-challenge/bistro-bliss.png',
+    githubUrl: 'https://github.com/denetodev/bistro-bliss',
+    siteUrl: 'https://bistro-bliss-amber.vercel.app/',
+    isHovered: false,
+  },
+  {
+    id: '4',
+    title: 'Menu Restaurante',
+    description:
+      'Aplicação web responsiva que simula pedidos online, com seleção de pratos, ajustes de quantidades e cálculo interativo do total.',
+    image: '../../assets/images/card-challenge/menu-restaurante.png',
+    githubUrl: 'https://github.com/denetodev/BootCampDevJr_Projeto-Menu',
+    siteUrl: 'https://boot-camp-dev-jr-projeto-menu.vercel.app/',
     isHovered: false,
   },
 ];
